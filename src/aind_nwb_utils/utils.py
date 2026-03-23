78f716a569001072ab3f61429c17b8b7e442f2b6
arielleleon <54869022+arielleleon@users.noreply.github.com>
2026-03-23 11:58:23 -0700

    Fix experimenters name (#42)
    * fix: add experimenter full name to NWB file from the session/acquistion.json
    
    * fix: miswrote the join function - fixed now
    
    * fix: don't cast a string to a string
    
    * fix: because I am a dummy
    
    * test: test hardcoded string value to see if my docker build is gas lighting me
    
    * test: cast experimenters to list
    
    * fix: remove experimenter name
    
    * style: lint

--- src/aind_nwb_utils/utils.py
+++ src/aind_nwb_utils/utils.py
@@ -484,7 +484,6 @@ def create_base_nwb_file(data_path: Path) -> pynwb.NWBFile:
 
     data_description = metadata_map[data_path / "data_description.json"]
     subject_metadata = metadata_map[data_path / "subject.json"]
-    procedures_metadata = metadata_map[data_path / "procedures.json"]
     processing_metadata = metadata_map[data_path / "processing.json"]
     session_metadata = metadata_map[session_or_acquisition_path]
 
@@ -492,15 +491,6 @@ def create_base_nwb_file(data_path: Path) -> pynwb.NWBFile:
     session_start_date_time = _get_session_start_date_time(
         data_description["creation_time"]
     )
-
-    experimenters = [
-        procedure.get("experimenter_full_name")
-        for procedure in procedures_metadata.get("subject_procedures", [])
-    ]
-    experimenters += [
-        investigator.get("name", "No Experimenter Name")
-        for investigator in data_description.get("investigators", [])
-    ]
 
     generation_code = [
         process.get("code")
@@ -531,7 +521,6 @@ def create_base_nwb_file(data_path: Path) -> pynwb.NWBFile:
         institution=data_description["institution"].get("name", None),
         subject=nwb_subject,
         session_id=data_description["name"],
-        experimenter=str(experimenters),
         lab=data_description.get("group", ""),
     )
 
